c7be853c69db3b65db10730ccb421ab74a49bfb4
ArdorHoon <didgnsah7320@gmail.com>
2022-12-01 10:43:24 +0900

    init

---
+++ src/main/java/com/jumptospring/example/MysiteApplication.java
@@ -0,0 +1,13 @@
+package com.jumptospring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysiteApplication.class, args);
+    }
+
+}
