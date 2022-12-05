eb4300054619b15e71b28397e825cfe6d7e99089
ArdorHoon <didgnsah7320@gmail.com>
2022-12-05 14:15:34 +0900

    add service and detail question

--- src/main/java/com/jumptospring/example/question/QuestionController.java
+++ src/main/java/com/jumptospring/example/question/QuestionController.java
@@ -3,23 +3,33 @@ package com.jumptospring.example.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 
+@RequestMapping("/question")
 @RequiredArgsConstructor //롬복이 제공하는 애너테이션으로 final이 붙은 속성을 포함하는 생성자를 자동으로 생성하는 역할
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         //Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다. Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 
 }
---
+++ src/main/java/com/jumptospring/example/question/QuestionService.java
@@ -0,0 +1,32 @@
+package com.jumptospring.example.question;
+
+
+import com.jumptospring.example.error.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        if(question.isPresent()){
+            return question.get();
+        }else{
+            throw new DataNotFoundException("question Not Found");
+        }
+
+    }
+}
