b48a997931c4576c6b7100682cfaf8f1273d29c3
ArdorHoon <didgnsah7320@gmail.com>
2022-12-14 11:17:10 +0900

    add question delete

--- src/main/java/com/jumptospring/example/question/QuestionController.java
+++ src/main/java/com/jumptospring/example/question/QuestionController.java
@@ -98,4 +98,16 @@ public class QuestionController {
         return String.format("redirect:/question/detail/%s", id);
     }
 
+    @PreAuthorize("isAuthenticated()") //로그아웃 상태에서는 로그인 페이지로 간다.
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id){
+        Question question = this.questionService.getQuestion(id);
+
+        if(!question.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/jumptospring/example/question/QuestionService.java
+++ src/main/java/com/jumptospring/example/question/QuestionService.java
@@ -54,4 +54,8 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         this.questionRepository.save(question);
     }
+
+    public void delete(Question question){
+        this.questionRepository.delete(question);
+    }
 }
